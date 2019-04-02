04afa82519dd16bb8294ec8854e3d5050acff7dd
Ismet Okatar <42450732+okatarismet@users.noreply.github.com>
2019-04-02 16:44:43 +0300

    added

---
+++ ws-client.js
@@ -0,0 +1,33 @@
+var ws = new WebSocket("ws://localhost:3000");
+
+
+ws.onopen = function() {
+    setTitle(`Connected to Cyber Chat `);
+  
+};
+
+ws.onclose = function() {
+	setTitle("DISCONNECTED");
+};
+
+ws.onmessage = function(payload) {
+	printMessage(payload.data);
+};
+
+document.forms[0].onsubmit = function () {
+    var input = document.getElementById('message');
+    var user = document.getElementById('user');
+    ws.send(">"+ user.value+ ": " + input.value);
+   
+    input.value = '';
+};
+
+function setTitle(title) {
+    document.querySelector('h1').innerHTML = title;
+}
+
+function printMessage(message) {
+    var p = document.createElement('p');
+    p.innerText = message;
+    document.querySelector('div.messages').appendChild(p);
+}
